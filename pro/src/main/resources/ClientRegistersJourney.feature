530051f061ce724b9be0c6169d5d2497787880d9
thomasfayad <thomasfayad@gmail.com>
2020-04-21 21:04:26 +0000

    pro + java main

--- pro/src/main/resources/ClientRegistersJourney.feature
+++ pro/src/main/resources/ClientRegistersJourney.feature
@@ -20,7 +20,7 @@
 Feature: User registers containers
 
 Background: 
-		Given a client "danish porks"
+		Given a client "Netto"
 		And a journey database
 
   @tag1
@@ -28,7 +28,7 @@ Background:
     Given a valid port of origin "Copenhaguen"
     And a valid destination "Beirut"
     And a valid content "Pork meat"
-    And a valid company "danish porks"
+    And a valid company "Netto"
     When Registering
     Then the system displays a message confirming the registration
     And a new journey id is automaticly generated
@@ -37,7 +37,7 @@ Background:
    Scenario: The client does not give the content of the container's journey
 	  Given a valid port of origin "Copenhaguen"
 	  And a valid destination "Beirut"
-    And a valid company "danish porks"
+    And a valid company "Netto"
 	  When Registering
 	  Then the system displays a message telling that the content needs to be filled
 	  
--- pro/src/test/java/pro/StepDefinition.java
+++ pro/src/test/java/pro/StepDefinition.java
@@ -5,22 +5,43 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class StepDefinition {
-
-
+	
+//Feature: Client Find Journey
+	Client c;
+	Journey j = new Journey();
+	clientDatabase CD;
+	journeyDB JD;
+	String searchword;
+	String filter;
+	ResponseObject searchResponse;
+	
 @Given("a client {string}")
-public void a_client(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+public void a_client(String client) {
+	Client c = new Client(client);
+	c.setClient(client);
 }
 
-@Given("a journey database with the client {string} {string} {string} and {string}")
-public void a_journey_database_with_the_client_and(String string, String string2, String string3, String string4) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+@Given("a client database")
+public void a_client_database() {
+	CD = c.clientDatabase;
+}
+
+//Background
+
+@Given("a journey database with the departing port {string} {string} and {string}")
+public void a_journey_database_with_the_departing_port_and(String dp1, String dp2, String dp3) {
+	Jou j1 = new Jou(dp1 ,"CPH","Veggies","Netto","JOU00001");
+    Jou j2 = new Jou(dp2, "Bey","oil","Total","JOU00002");
+    Jou j3 = new Jou(dp3,"NYC","pork","Meat Lovers","JOU00003");
+    
+    c.journeyDB.registering(dp1);
+    c.journeyDB.registering(dp2);
+    c.journeyDB.registering(dp3);
+    
 }
 
 @Given("a searchword {string}")
-public void a_searchword(String string) {
+public void a_searchword(String sword) {
     // Write code here that turns the phrase above into concrete actions
     throw new io.cucumber.java.PendingException();
 }
@@ -31,23 +52,21 @@ public void a_filter(String string) {
     throw new io.cucumber.java.PendingException();
 }
 
-@When("searching for client")
-public void searching_for_client() {
+@When("searching for journey")
+public void searching_for_journey() {
     // Write code here that turns the phrase above into concrete actions
     throw new io.cucumber.java.PendingException();
 }
 
-@Then("the corresponding client\\(s) is\\/are found.")
-public void the_corresponding_client_s_is_are_found() {
+@Then("the corresponding journey is\\/are found.")
+public void the_corresponding_journey_is_are_found() {
     // Write code here that turns the phrase above into concrete actions
     throw new io.cucumber.java.PendingException();
 }
 
+
+//Feature:
 @Given("a client {string}")
-public void a_client(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
 
 @Given("a journey database")
 public void a_journey_database() {
@@ -96,171 +115,22 @@ public void a_new_journey_id_is_automaticly_generated() {
     // Write code here that turns the phrase above into concrete actions
     throw new io.cucumber.java.PendingException();
 }
+//Feature:
 
 @Given("a client {string}")
-public void a_client(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
 
 @Given("a journey database")
-public void a_journey_database() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
 
 @Given("a valid port of origin {string}")
-public void a_valid_port_of_origin(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
 
 @Given("a valid destination {string}")
-public void a_valid_destination(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
 
 @Given("a valid company {string}")
-public void a_valid_company(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
 
 @When("Registering")
-public void registering() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
 
 @Then("the system displays a message telling that the content needs to be filled")
 public void the_system_displays_a_message_telling_that_the_content_needs_to_be_filled() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-
-@Given("a client {string}")
-public void a_client(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-
-@Given("a journey database")
-public void a_journey_database() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-
-@Given("a valid destination {string}")
-public void a_valid_destination(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-
-@Given("a valid content {string}")
-public void a_valid_content(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-
-@Given("a valid company {string}")
-public void a_valid_company(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-
-@When("Registering")
-public void registering() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-
-@Then("the system displays a message telling that the port of origin needs to be filled")
-public void the_system_displays_a_message_telling_that_the_port_of_origin_needs_to_be_filled() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-
-@Given("a client {string}")
-public void a_client(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-
-@Given("a journey database")
-public void a_journey_database() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-
-@Given("a valid port of origin {string}")
-public void a_valid_port_of_origin(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-
-@Given("a valid content {string}")
-public void a_valid_content(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-
-@Given("a valid company {string}")
-public void a_valid_company(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-
-@When("Registering")
-public void registering() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-
-@Then("the system displays a message telling that the pdestination needs to be filled")
-public void the_system_displays_a_message_telling_that_the_pdestination_needs_to_be_filled() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-
-@Given("a client {string}")
-public void a_client(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-
-@Given("a journey database")
-public void a_journey_database() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-
-@Given("a valid port of origin {string}")
-public void a_valid_port_of_origin(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-
-@Given("a valid destination {string}")
-public void a_valid_destination(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-
-@Given("a valid content {string}")
-public void a_valid_content(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-
-@When("Registering")
-public void registering() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-
-@Then("the system displays a message telling that the company needs to be filled")
-public void the_system_displays_a_message_telling_that_the_company_needs_to_be_filled() {
     // Write code here that turns the phrase above into concrete actions
     throw new io.cucumber.java.PendingException();
 }
@@ -294,5 +164,4 @@ public void the_journey_information_is_updated() {
     // Write code here that turns the phrase above into concrete actions
     throw new io.cucumber.java.PendingException();
 }
-
 }
