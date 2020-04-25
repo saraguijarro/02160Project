73aeb4a327fe039aaa9da188b6a639635a04db61
Mihai Nipomici <mihainipomici@gmail.com>
2020-04-25 18:11:51 +0300

    Add user repository with type as client or simple user. Added login method

--- schema.sql
+++ schema.sql
@@ -1,12 +1,10 @@
 #you can use this queries to create tables in the database to try to run the program
 
-CREATE TABLE client(
-                    ID INT PRIMARY KEY AUTO_INCREMENT,Name VARCHAR(255),
-                    Address VARCHAR(255),Person VARCHAR(255),Email VARCHAR(255));
 
 CREATE TABLE users(
                       id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                      email VARCHAR(100) NOT NULL,
+                      user_type ENUM('CLIENT', 'LOGISTIC_COMPANY') NOT NULL,
+                      email VARCHAR(100) UNIQUE NOT NULL,
                       password VARCHAR(100) NOT NULL,
                       created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -21,6 +19,7 @@ INSERT INTO actions(action_type) VALUES
 ('ADD_CONTAINER'),
 ('CONTAINER_HISTORY'),
 ('FIND_JOURNEY');
+
 
 CREATE TABLE user_actions(
                              id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
