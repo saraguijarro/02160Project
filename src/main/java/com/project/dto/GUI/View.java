95fd5017f8aa8c58c046af48ec79d9120596e0d0
Miguel <mbustorff@gmail.com>
2020-04-27 15:25:12 +0200

    Some classes in view

---
+++ src/main/java/com/project/dto/GUI/View.java
@@ -0,0 +1,32 @@
+
+public class View {
+	
+	
+	
+	
+	
+}
+
+class logInScreen {
+	
+}
+
+class clientMainMenuScreen {
+	
+}
+
+class compMainMenuScreen {
+	
+}
+
+class compClientsScreen {
+	
+}
+
+class compJourneyScreen {
+	
+}
+
+class 
+
+class 
